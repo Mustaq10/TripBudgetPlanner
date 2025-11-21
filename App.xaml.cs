d65e14739f8d5d3d21aed99d0a06da8c74115f4c
shaik <shaik@MUSTAQ>
2025-11-21 13:26:22 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TripBudgetPlanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
